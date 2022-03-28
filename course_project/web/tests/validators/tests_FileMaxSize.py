72c9402ca6d925203514429906efd12953be072d
KalinHar <kalix@abv.bg>
2022-03-28 09:49:08 +0300

    ref tests validators

--- course_project/web/tests/validators/tests_FileMaxSize.py
+++ course_project/web/tests/validators/tests_FileMaxSize.py
@@ -4,28 +4,27 @@ from django.test import TestCase
 from course_project.web.validators import file_max_size
 
 
-class BiggerSize:
-    size = 5.1 * 1024 *1024
+class FakeSize:
+    def __init__(self, size):
+        self.size = size
 
 
-class FakeBiggerFile:
-    file = BiggerSize()
+class FakeFile:
+    def __init__(self, file_size):
+        self.file = FakeSize(file_size)
 
 
-class NormalSize:
-    size = 5 * 1024 *1024
-
-
-class FakeNormalFile:
-    file = NormalSize()
+BIGGER_SIZE = 5.1 * 1024 * 1024
+NORMAL_SIZE = 5 * 1024 * 1024
 
 
 class FileMaxSizeTest(TestCase):
     def test_raise_error(self):
         with self.assertRaises(ValidationError) as error:
-            file_max_size(FakeBiggerFile())
+            file_max_size((FakeFile(BIGGER_SIZE)))
 
         self.assertIsNotNone(error)
 
     def test_not_raise_error(self):
-        self.assertEqual(file_max_size(FakeNormalFile()), None)
+
+        self.assertEqual(file_max_size((FakeFile(NORMAL_SIZE))), None)
--- course_project/web/tests/validators/tests_PhoneValidator.py
+++ course_project/web/tests/validators/tests_PhoneValidator.py
@@ -5,11 +5,12 @@ from course_project.web.validators import phone_validator
 
 
 class PhoneValidatorTest(TestCase):
-    def test_raise_errors(self):
+    def test_raise_error_min_length(self):
         with self.assertRaises(ValidationError) as error:
             phone_validator('1234567')
         self.assertIsNotNone(error)
 
+    def test_raise_error_not_all_numbers(self):
         with self.assertRaises(ValidationError) as error:
             phone_validator('12345680r7')
         self.assertIsNotNone(error)
