1a10a0a9324bf6cd405d4ee6acecf878d0b5bb8b
KalinHar <kalix@abv.bg>
2022-04-11 14:34:27 +0300

    add 403 to middleware handler exeption

--- course_project/web/middleware.py
+++ course_project/web/middleware.py
@@ -1,4 +1,4 @@
-from course_project.web.views.error_page import NotExistPageView, ServerErrorPageView
+from course_project.web.views.error_page import NotExistPageView, ServerErrorPageView, ForbiddenPageView
 
 
 # def handle_exception(get_response):
@@ -20,6 +20,8 @@ class HandleException:
         response = self.get_response(request)
         if response.status_code == 404:
             return NotExistPageView.as_view()(request)
+        elif response.status_code == 403:
+            return ForbiddenPageView.as_view()(request)
         elif response.status_code >= 500:
             return ServerErrorPageView.as_view()(request)
         return response
